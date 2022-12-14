6b60c8bf8fbb2f999ab62f1d2592be93210666b6
faria-karim-porna <faria.porna.08@gmail.com>
2022-12-14 09:38:35 +0000

    About Loop Added

---
+++ basic/AboutLoops/main.cpp
@@ -0,0 +1,111 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "The For Loop Iteration Number Is " << i << endl;
+    }
+
+    int j = 0;
+    while (j < 5)
+    {
+        cout << "The While Loop Iteration Number Is " << j << endl;
+        j++;
+    }
+
+    int k = 0;
+    do
+    {
+        cout << "The Do While Loop Iteration Number Is " << k << endl;
+        k++;
+    } while (k > 0 && k < 5);
+
+    for (int a = 0; a < 2; a++)
+    {
+        for (int b = 0; b < 2; b++)
+        {
+            cout << "The Nested For Loop Iteration Number Is " << a << " and " << b << endl;
+        }
+    }
+
+    int c = 0;
+    while (c < 2)
+    {
+        int d = 0;
+        while (d < 2)
+        {
+            cout << "The Nested While Loop Iteration Number Is " << c << " and " << d << endl;
+            d++;
+        }
+        c++;
+    }
+
+    int e = 0;
+    do
+    {
+        int f = 0;
+
+        do
+        {
+            cout << "The Nested Do While Loop Iteration Number Is " << e << " and " << f << endl;
+            f++;
+        } while (f > 0 && f < 2);
+        e++;
+    } while (e > 0 && e < 2);
+
+    for (int l = 0; l < 5; l++)
+    {
+        cout << "The Break Statement Iteration Number Is " << l << endl;
+        if (l == 3)
+        {
+            break;
+        }
+    }
+
+    for (int m = 0; m < 10; m++)
+    {
+        if (m % 3 == 0)
+        {
+            continue;
+        }
+        cout << "The Continue Statement Iteration Number Is " << m << endl;
+    }
+
+    int p = 0;
+jump:
+    for (int n = 0; n <= 15; n++)
+    {
+        // PVALUE:
+        //     p = 0;
+        if (n % 3 == 0)
+        {
+            goto jump;
+        }
+        // if (n % 5 == 0)
+        // {
+        //     goto PVALUE;
+        // }
+        cout << "The Goto Statement Iteration Number Is " << n << endl;
+        cout << "The Goto Statement Iteration Number For P Is " << p << endl;
+        p++;
+    }
+
+    for (;;)
+    {
+        cout << "The Infinite For Loop" << endl;
+    }
+
+    while (1)
+    {
+        cout << "The Infinite While Loop" << endl;
+    }
+
+    do
+    {
+        cout << "The Infinite Do While Loop" << endl;
+    } while (1);
+
+    return 0;
+}
